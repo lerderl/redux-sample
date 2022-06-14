d20ee7214922416a50ad618501c4467c3e4e1f05
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-06-14 16:17:40 +0100

    Converting todos and goals to context

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Context = React.createContext();
+
+const Parent = () => {
+  return (
+    <div>
+      <h1>Parent</h1>
+      <Child />
+    </div>
+  );
+};
+
+const Child = () => {
+  return (
+    <div>
+      <h1>Child</h1>
+      <Grandchild />
+    </div>
+  );
+};
+
+const Grandchild = ({ name }) => {
+  return (
+    <Context.Consumer>
+      {name => (
+        <div>
+          <h1>Grandchild</h1>
+          <h3>Name: {name}</h3>
+        </div>
+      )}      
+    </Context.Consumer>
+  );
+};
+
+const App = () => {
+  const name = "Andrew";
+  
+  return (
+    <Context.Provider value={name}>
+      <Parent />
+    </Context.Provider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
